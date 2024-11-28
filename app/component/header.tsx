25fa95c8162f504ca61713cca91c7882f0e3f7f9
eskander-taher <eskander.alshaibany@gmail.com>
2024-11-29 01:55:15 +0300

    init commit

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { SignedIn, UserButton } from "@clerk/nextjs";
-import { useUser } from "@clerk/nextjs";
+// import { useUser } from "@clerk/nextjs";
 // import Link from "next/link";
 
 export default function Header() {
-	const { user } = useUser();
+	// const { user } = useUser();
 	return (
 		<header className="absolute w-full bg-slate-900 flex justify-end items-center gap-10 px-10 py-4">
 			<SignedIn>
