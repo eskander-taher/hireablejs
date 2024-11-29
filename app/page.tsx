28b216b229b14b00756bc9c9ef691d94c88b5930
eskander-taher <eskander.alshaibany@gmail.com>
2024-11-29 16:27:11 +0300

    add clerk react

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { SignedOut, SignedIn, SignInButton, useUser } from "@clerk/nextjs";
+import { SignedOut, SignedIn, SignInButton, useUser } from "@clerk/clerk-react";
 
 export default function Home() {
 	const { user } = useUser();
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
 			<h1 className=" text-6xl">HireableJS</h1>
-			<h1>A zone for all desperate JavaScript developers</h1>
+			<h1>The zone for all desperate JavaScript developers</h1>
 
 			<SignedOut>
 				<h1>Join the waiting list, be one of the first to use these game changing tools</h1>
