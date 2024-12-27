e55a5c192e40558eed6a1af514dd72429be188a8
eskander-taher <eskander.alshaibany@gmail.com>
2024-12-28 00:00:16 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
 			<h1 className=" text-6xl">HireableJS</h1>
-			<h1>The zone for all desperate JavaScript developers</h1>
+			<h1>The zone for all desperate JavaScript developers.</h1>
 
 			<SignedOut>
 				<h1>Join the waiting list, be one of the first to use these game changing tools</h1>
