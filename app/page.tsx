7e467e440c69a7f74cf2cecd7e057e411dd2f15c
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-01 13:08:01 +0300

    add clerk to prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import axios from "axios";
-import { BASE_URL } from "@/constants";
-
-export default async function Home() {
-	const res = await axios.get(`${BASE_URL}/users`);
+export default  function Home() {
 
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
 			<h1 className=" text-6xl">HireableJS</h1>
 			<h1>The zone for all desperate JavaScript developers.</h1>
-
-			{res.data && (
-				<p>
-					Until now <span className=" font-bold text-xl">{res.data.length}</span>{" "}
-					JavaScript developers have joined the community.
-				</p>
-			)}
 		</div>
 	);
 }
