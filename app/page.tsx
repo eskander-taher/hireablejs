38861191faca8affb115fef49d03876bd6f3e61f
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-01 12:54:10 +0300

    add clerk to prod

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
 			<h1 className=" text-6xl">HireableJS</h1>
-			<h1>The zone for all desperate JavaScript developers</h1>
+			<h1>The zone for all desperate JavaScript developers.</h1>
 
 			{res.data && (
 				<p>
 					Until now <span className=" font-bold text-xl">{res.data.length}</span>{" "}
-					JavaScript developers have joined the community
+					JavaScript developers have joined the community.
 				</p>
 			)}
 		</div>
